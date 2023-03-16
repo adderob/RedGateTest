5512f231b7d932de8003188f0640a5c6e8823a09
Rob Nichols <rob.nichols@amwins.com>
2023-03-16 15:39:54 -0400

    Set PrimaryKey for Market table and set Name and Code to NOT NULL

--- Tables/dbo.Market.sql
+++ Tables/dbo.Market.sql
@@ -1,7 +1,9 @@
 CREATE TABLE [dbo].[Market]
 (
 [Id] [int] NOT NULL IDENTITY(1, 1),
-[Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Code] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Code] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Market] ADD CONSTRAINT [PK_Market] PRIMARY KEY CLUSTERED ([Id]) ON [PRIMARY]
 GO
